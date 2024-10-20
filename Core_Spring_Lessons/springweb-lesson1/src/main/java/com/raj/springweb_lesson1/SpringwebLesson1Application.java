8230aed2a621967f080ce90616dc363cef22eb3f
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2024-10-20 22:01:09 +0800

    springweb lesson 1 initialization

---
+++ Core_Spring_Lessons/springweb-lesson1/src/main/java/com/raj/springweb_lesson1/SpringwebLesson1Application.java
@@ -0,0 +1,13 @@
+package com.raj.springweb_lesson1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebLesson1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebLesson1Application.class, args);
+	}
+
+}
---
+++ Core_Spring_Lessons/springweb-lesson1/src/test/java/com/raj/springweb_lesson1/SpringwebLesson1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raj.springweb_lesson1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebLesson1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
