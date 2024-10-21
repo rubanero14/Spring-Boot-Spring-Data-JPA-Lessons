8bf933b90eeba0e6b3d901ecedd102aceb67954c
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2024-10-21 08:54:26 +0800

    Spring boot custom banner setup

--- Core_Spring_Lessons/springweb-lesson1/src/main/java/com/raj/springweb_lesson1/SpringwebLesson1Application.java
+++ Core_Spring_Lessons/springweb-lesson1/src/main/java/com/raj/springweb_lesson1/SpringwebLesson1Application.java
@@ -8,6 +8,6 @@ public class SpringwebLesson1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringwebLesson1Application.class, args);
+		System.out.println("System online...");
 	}
-
 }
