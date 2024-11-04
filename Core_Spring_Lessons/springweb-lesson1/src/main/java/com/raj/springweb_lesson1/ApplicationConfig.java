2c256a65f6d72b02928521c05a76b1716ba3d87b
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2024-11-04 21:58:22 +0800

    Update ApplicationConfig.java

--- Core_Spring_Lessons/springweb-lesson1/src/main/java/com/raj/springweb_lesson1/ApplicationConfig.java
+++ Core_Spring_Lessons/springweb-lesson1/src/main/java/com/raj/springweb_lesson1/ApplicationConfig.java
@@ -3,7 +3,10 @@ package com.raj.springweb_lesson1;
 import org.springframework.context.annotation.*;
 
 
-// Best practice for individually creating instances using config files separately using @Configuration
+/*
+	Best practice for individually creating instances using config files separately using @Configuration so that Spring
+	watches for it for start up
+*/
 @Configuration
 public class ApplicationConfig {
     	// Spring instance creation called Bean for DI
